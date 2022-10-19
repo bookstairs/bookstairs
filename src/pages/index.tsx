e2e1e35e6bed160d1e317799b8d06c48aedc10fb
syhily <syhily@gmail.com>
2022-10-20 01:32:30 +0800

    chore: remove useless welcome page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,10 @@ import { Group } from '@mantine/core';
 import ColorSchemeToggle from '@/components/Layout/Header/ColorSchemeToggle';
 import LanguageSwitch from '@/components/Layout/Header/LanguageSwitch';
 import LoadingOverlay from '@/components/LoadingOverlay';
-import { Welcome } from '@/components/Welcome/Welcome';
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
       <Group position="center" mt="xl">
         <ColorSchemeToggle />
       </Group>
